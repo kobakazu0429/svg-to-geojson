bb88fb039c3d443bcb7ce85350c7090b04370b8f
kobakakzu0429 <kobakazu10@gmail.com>
2021-01-21 17:16:56 +0900

    update

--- src/path-to-coordinates.js
+++ src/path-to-coordinates.js
@@ -2,14 +2,14 @@ import { range } from 'lodash';
 
 function pathToCoords(path, scale, numPoints, translateX, translateY) {
   const length = path.getTotalLength();
-  const getRange = range(numPoints);
+  const getRange = [... new Array(numPoints).keys()];
   // Always include the max value in the range.
   // This is helpful for detecting closed polygons vs lines
   getRange.push(numPoints);
 
   return {
     path,
-    coords: getRange.map(function(i) {
+    coords: getRange.map(function (i) {
       const point = path.getPointAtLength(length * i / numPoints);
       return [point.x * scale + translateX, point.y * scale + translateY];
     })
