3094349f84bcc504b82702aaf21d2ffd368450b6
kobakakzu0429 <kobakazu10@gmail.com>
2021-01-24 11:52:57 +0900

    remove unnecessary path attributes, through pathologist

--- src/pathologize.ts
+++ src/pathologize.ts
@@ -1,11 +1,21 @@
-import { transform } from "pathologist/dist/index.modern";
+import { parse } from "pathologist/dist/index.modern";
 
 function pathologize(svg: string): Promise<string> {
   const expression = /<(text|style|metadata|pattern)[\s\S]*?<\/(text|style|metadata|pattern)>/g;
   const clean = svg.replace(expression, "");
 
   return new Promise((resolve, reject) => {
-    const transformed = transform(clean);
+    const parsed = parse(clean);
+
+    parsed.paths = parsed.paths.map((path) => {
+      delete path["stroke-linecap"];
+      delete path["stroke-linejoin"];
+      delete path["stroke-miterlimit"];
+      delete path["stroke-width"];
+      return path;
+    });
+
+    const transformed = parsed.toString();
     if (!transformed) return reject(svg);
     resolve(transformed);
   });
--- src/types/pathologist.d.ts
+++ src/types/pathologist.d.ts
@@ -1,3 +1,9 @@
 declare module "pathologist/dist/index.modern" {
   export function transform(path: string): string;
+  export function parse(
+    path: string
+  ): {
+    paths: Record<string, string>[];
+    toString(): string;
+  };
 }
